6400e4647b6ea12ec70ad19c222a2677b305ac8f
albetnov <ryxinfrvr@gmail.com>
2022-12-15 17:00:19 +0700

    add support for pagination & fix wrong date

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -7,6 +7,7 @@ interface CardProps {
   header: string;
   content: string;
   key: string;
+  date: string;
 }
 
 const Card: Component<CardProps> = (props) => {
@@ -14,7 +15,7 @@ const Card: Component<CardProps> = (props) => {
     <div class="p-3 border-b border-b-slate-300 mt-3">
       <div class="flex gap-5 items-center mb-3">
         <Tag>{props.tags}</Tag>
-        <div class="text-slate-500">{new Date().toLocaleDateString()}</div>
+        <div class="text-slate-500">{props.date}</div>
       </div>
       <h3 class="text-2xl font-bold text-zinc-800">{props.header}</h3>
       <p class="text-lg text-zinc-500">{props.content}</p>
--- src/Pages/News.tsx
+++ src/Pages/News.tsx
@@ -6,8 +6,8 @@ import HighlightLoading from "../Components/News/HighlightLoading";
 import { ApiReturnProps } from "../Utils/Api";
 
 interface NewsProps {
-  firstMethod: () => Promise<ApiReturnProps[]>;
-  secondMethod?: (() => Promise<ApiReturnProps[]>) | null;
+  firstMethod: (page?: number) => Promise<ApiReturnProps[]>;
+  secondMethod?: ((page?: number) => Promise<ApiReturnProps[]>) | null;
   title: string;
 }
 
@@ -15,70 +15,116 @@ const News: Component<NewsProps> = ({ firstMethod, secondMethod = null, title })
   const [firstHalf, setFirstHalf] = createSignal<ApiReturnProps[]>([]);
   const [lastHalf, setLastHalf] = createSignal<ApiReturnProps[]>([]);
   const [highlight, setHighlight] = createSignal<ApiReturnProps | object>({});
+  const [currentPage, setCurrentPage] = createSignal(1);
+  const [somethingWrong, setSomethingWrong] = createSignal(false);
+  const [isLoading, setIsLoading] = createSignal(false);
 
-  const fetchNews = async () => {
-    const result = await firstMethod();
+  const onNextPageHandler = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+
+  createEffect(async () => {
+    setIsLoading(true);
+    await fetchNews(currentPage());
+    setIsLoading(false);
+  });
+
+  const fetchNews = async (page = 1) => {
+    const result = await firstMethod(page);
+    if (!result) {
+      setSomethingWrong(true);
+      return;
+    }
     setHighlight(result[0]);
     setFirstHalf(result.slice(1, 4));
     if (secondMethod === null) {
       setLastHalf(result.slice(4, 8));
     } else {
-      const result2 = await secondMethod();
+      const result2 = await secondMethod(page);
+      if (!result2) {
+        setSomethingWrong(true);
+        return;
+      }
       setLastHalf(result2.slice(0, 3));
     }
   };
 
-  onMount(() => {
-    fetchNews();
-  });
-
   return (
-    <section class="p-3">
-      <h2 class="text-4xl font-bold mb-10 mt-3 mx-5">{title}</h2>
-      <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 items-start">
-        <div class="min-h-full lg:border-r border-r-slate-400">
-          <Show when={Object.keys(highlight()).length > 0} fallback={<HighlightLoading />}>
-            <Highlight highlight={highlight() as ApiReturnProps} />
-          </Show>
+    <Show
+      when={!somethingWrong()}
+      fallback={
+        <div class="h-screen w-full flex items-center justify-center text-4xl font-bold">
+          404: Ups Next Page not found.
         </div>
-        <div class="lg:border-r pr-5 border-r-slate-400 min-h-full">
-          <Show
-            when={firstHalf().length > 0}
-            fallback={
-              <>
-                <CardLoading />
-                <CardLoading />
-                <CardLoading />
-              </>
-            }
-          >
-            <For each={firstHalf()}>
-              {(item) => (
-                <Card key={item.key} tags={item.tag} header={item.title} content={item.desc} />
-              )}
-            </For>
-          </Show>
+      }
+    >
+      <section class="p-3">
+        <h2 class="text-4xl font-bold mb-10 mt-3 mx-5">{title}</h2>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 items-start">
+          <div class="min-h-full lg:border-r border-r-slate-400">
+            <Show when={!isLoading()} fallback={<HighlightLoading />}>
+              <Highlight highlight={highlight() as ApiReturnProps} />
+            </Show>
+          </div>
+          <div class="lg:border-r pr-5 border-r-slate-400 min-h-full">
+            <Show
+              when={!isLoading()}
+              fallback={
+                <>
+                  <CardLoading />
+                  <CardLoading />
+                  <CardLoading />
+                </>
+              }
+            >
+              <For each={firstHalf()}>
+                {(item) => (
+                  <Card
+                    date={item.time}
+                    key={item.key}
+                    tags={item.tag}
+                    header={item.title}
+                    content={item.desc}
+                  />
+                )}
+              </For>
+            </Show>
+          </div>
+          <div class="min-h-full">
+            <Show
+              when={!isLoading()}
+              fallback={
+                <>
+                  <CardLoading />
+                  <CardLoading />
+                  <CardLoading />
+                </>
+              }
+            >
+              <For each={lastHalf()}>
+                {(item) => (
+                  <Card
+                    date={item.time}
+                    key={item.key}
+                    tags={item.tag}
+                    header={item.title}
+                    content={item.desc}
+                  />
+                )}
+              </For>
+            </Show>
+          </div>
         </div>
-        <div class="min-h-full">
-          <Show
-            when={lastHalf().length > 0}
-            fallback={
-              <>
-                <CardLoading />
-                <CardLoading />
-                <CardLoading />
-              </>
-            }
-          >
-            <For each={lastHalf()}>
-              {(item) => (
-                <Card key={item.key} tags={item.tag} header={item.title} content={item.desc} />
-              )}
-            </For>
-          </Show>
-        </div>
-      </div>
-    </section>
+        <button
+          onClick={onNextPageHandler}
+          class={`py-3 px-5 bg-zinc-700 hover:bg-zinc-800 active:opacity-80 text-white border border-slate-400 rounded-lg fixed right-3 bottom-5 ${
+            isLoading() ? "animate-bounce" : ""
+          }`}
+        >
+          {isLoading() ? "Loading..." : "Next"}
+        </button>
+      </section>
+    </Show>
   );
 };
 
